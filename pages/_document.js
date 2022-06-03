5182fb5baaa073bca2eaafc2d6301139f38a64a9
Michael Podolsky <michaelspodolsky@gmail.com>
2022-06-02 21:12:54 -0700

    init repository

---
+++ pages/_document.js
@@ -0,0 +1,29 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:wght@400;500;700;900&display=swap"
+          rel="stylesheet"
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lobster&display=swap"
+          rel="stylesheet"
+        ></link>
+        <title>Cook50</title>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "~styles/pages/index.module.scss";
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Cook50</h1>
+    </div>
+  );
+};
+
+export default Home;
